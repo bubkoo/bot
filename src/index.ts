05d36db90d6c2c5deb50a62ff56f17c39a2c44c5
bubkoo <bubkoo.wy@gmail.com>
2020-09-16 20:55:35 +0800

    feat: ✨ start app

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-import { Application } from "probot"
+import { Probot } from "probot"
+import app from "./app"
 
-export = (app: Application) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    })
-    await context.github.issues.createComment(issueComment)
-  })
-
-  app.on("issue_comment.created", async (context) => {
-    const issueComment = context.issue({
-      body: "issue commneted",
-    })
-    await context.github.issues.createComment(issueComment)
-  })
-}
+Probot.run(app)
