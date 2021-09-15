24f739d0134d43b4c2e226d165988da18264dea6
bubkoo <bubkoo.wy@gmail.com>
2021-09-15 17:32:16 +0800

    feat: ✨ add wip

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,10 @@
-import { Application } from 'probot'
-// import { PRTriage } from './pr-triage'
-// import { Welcome } from './welcome'
-// import { AutoAssign } from './auto-assign'
-// import { AutoComment } from './auto-comment'
-// import { RequestInfo } from './request-info'
-// import { LabelActions } from './label-actions'
+import { Application, Context } from 'probot'
+import { WIP } from './wip'
 
 export = (app: Application) => {
-  // PRTriage.start(app)
-  // Welcome.start(app)
-  // AutoComment.start(app)
-  // AutoAssign.start(app)
-  // RequestInfo.start(app)
-  // LabelActions.start(app)
+  app.on('*', async (context: Context) => {
+    context.log(`event: ${context.name}`)
+  })
+
+  WIP.start(app)
 }
