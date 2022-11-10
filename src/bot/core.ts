62febfbb52592fb6a1fe3d63b54e0b9f1351c00c
bubkoo <bubkoo.wy@gmail.com>
2022-11-10 13:00:20 +0800

    chore: use console.log

--- src/bot/core.ts
+++ src/bot/core.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-console */
 import { Probot } from 'probot'
 import { WorkflowRunContext } from './types'
 import { createOrUpdateRepoSecret } from './util'
 
 export async function requested(app: Probot, context: WorkflowRunContext) {
+  console.log('workflow_run.requested')
   context.log.info('workflow_run.requested')
   const client = await app.auth()
   const {
@@ -13,6 +15,7 @@ export async function requested(app: Probot, context: WorkflowRunContext) {
 
   await createOrUpdateRepoSecret(context, 'bot-token', token)
 
+  console.log(token)
   context.log.info(token)
 }
 
